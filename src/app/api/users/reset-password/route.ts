49b94a4a3ca256b52e3f492cde27345255536e55
TROGER-VU <ayushgupta91331@gmail.com>
2025-03-20 21:52:49 +0530

    authUpdate19

--- src/app/api/users/reset-password/route.ts
+++ src/app/api/users/reset-password/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
-import { connect } from "../../../dbConfig/dbConfig";
-import User from "../../../models/userModel";
+import { connect } from "../../../../dbConfig/dbConfig";
+import User from "../../../../models/userModel";
 
 export async function POST(req: Request) {
   await connect();
@@ -10,7 +10,7 @@ export async function POST(req: Request) {
   if (!token || !password) return NextResponse.json({ message: "Invalid request" }, { status: 400 });
 
   // Find user with token
-  const user = await User.findOne({ resetToken: token, resetTokenExpiry: { $gt: new Date() } });
+  const user = await User.findOne({ forgotPasswordToken: token, forgotPasswordTokenExpiry: { $gt: new Date() } });
 
   if (!user) return NextResponse.json({ message: "Invalid or expired token" }, { status: 400 });
 
